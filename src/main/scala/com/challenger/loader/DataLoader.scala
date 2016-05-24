54ff33ffb0656bcc4a57fc17f23cb342ae53ab49
Bill Kuang <bkuang@linkedin.com>
2016-05-23 20:30:55 -0700

    fix typo in trainingSet loader

--- src/main/scala/com/challenger/loader/DataLoader.scala
+++ src/main/scala/com/challenger/loader/DataLoader.scala
@@ -8,7 +8,7 @@ import scala.io.Source
 object DataLoader extends DataParser {
 
   def loadTrainingSet(): Seq[TrainingSetLine] = {
-    val source = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("head-training.tsv"))
+    val source = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("training.tsv"))
     try {
       source.getLines().to[Vector] map { parseTrainingSet }
     } finally {
