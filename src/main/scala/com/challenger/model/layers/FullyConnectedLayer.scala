de651cefb9cad99278cb603656eb06f6bb423942
Bill Kuang <bkuang@linkedin.com>
2016-05-24 17:13:38 -0700

    weightT => weight

--- src/main/scala/com/challenger/model/layers/FullyConnectedLayer.scala
+++ src/main/scala/com/challenger/model/layers/FullyConnectedLayer.scala
@@ -26,8 +26,8 @@ object FullyConnectedLayer {
       m: Int) = {
     new FullyConnectedLayer(
       DenseMatrix.create(
-        rows = numOutputs,
-        cols = numInputs,
+        rows = numInputs,
+        cols = numOutputs,
         data = Array.fill(numInputs * numOutputs) { Random.nextDouble() }),
       DenseVector.zeros[Double](numOutputs),
       activationFunction,
@@ -38,7 +38,7 @@ object FullyConnectedLayer {
 }
 
 class FullyConnectedLayer(
-    weightsT: DenseMatrix[Double],
+    weights: DenseMatrix[Double],
     biases: DenseVector[Double],
     activationFunction: DifferentiableFunction[Double, Double],
     alpha: Double,
@@ -48,11 +48,11 @@ class FullyConnectedLayer(
   private[this] var _activations = Option.empty[DenseVector[Double]]
   private[this] var _activationPrimes = Option.empty[DenseVector[Double]]
 
-  private val weightGrad = DenseMatrix.zeros[Double](rows = weightsT.rows, cols = weightsT.cols)
+  private val weightGrad = DenseMatrix.zeros[Double](rows = weights.rows, cols = weights.cols)
   private val biasGrad = DenseVector.zeros[Double](biases.length)
 
   def forward(inputs: DenseVector[Double]): DenseVector[Double] = {
-    val z = (weightsT * inputs) + biases
+    val z = (weights * inputs) + biases
     _activations = Some(z map activationFunction.apply)
     _activationPrimes = Some(z map activationFunction.prime)
     _activations.get
@@ -60,7 +60,7 @@ class FullyConnectedLayer(
 
   /**
     * Takes in the gradient from previous layer, computes the gradient for this layer and return it.
-    * This method will also update the weightsT for this layer for the next iteration of forward propagation.
+    * This method will also update the weights for this layer for the next iteration of forward propagation.
     *
     * @param losses These are losses from the previous layer. Previous layer = layer 1 CLOSER to the output layer
     * @return gradient/losses for this layer.
@@ -72,7 +72,7 @@ class FullyConnectedLayer(
     }
 
     // compute loss for this layer. this value will be returned so that next layer of neurons can continue to backward propagate.
-    val delta = (weightsT.t * losses) :* _activationPrimes.get
+    val delta = (weights.t * losses) :* _activationPrimes.get
 
     // partial derivatives to update the gradient
     val gradientDeltas = losses * _activations.get.t
@@ -83,7 +83,7 @@ class FullyConnectedLayer(
     biasGrad += biasDeltas
 
     // update the weights based on gradient and regularization
-    weightsT -= alpha * ((weightGrad / m.toDouble) + (weightsT * lambda))
+    weights -= alpha * ((weightGrad / m.toDouble) + (weights * lambda))
 
     // update the biases based on gradient
     biases -= alpha * (biasGrad / m.toDouble)
