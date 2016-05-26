108f36777710866234634c488b16e0166324070b
Bill Kuang <bkuang@linkedin.com>
2016-05-25 18:58:52 -0700

    refactor dataloader

--- src/main/scala/com/challenger/loader/DataLoader.scala
+++ src/main/scala/com/challenger/loader/DataLoader.scala
@@ -1,27 +1,22 @@
 package com.challenger.loader
 
-import com.challenger.data.{TestSetLine, TrainingSetLine}
+import com.challenger.data.{Line, TestSetLine, TrainingSetLine}
 import com.challenger.parser.DataParser
 
 import scala.io.Source
 
 object DataLoader extends DataParser {
 
-  def loadTrainingSet(): Seq[TrainingSetLine] = {
-    val source = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("training.tsv"))
+  def load[T <: Line](classpath: String)(f: String => T) = {
+    val source = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream(classpath))
     try {
-      source.getLines().to[Vector] map { parseTrainingSet }
+      source.getLines().to[Vector] map f
     } finally {
       source.close()
     }
   }
 
-  def loadTestSet(): Seq[TestSetLine] = {
-    val source = Source.fromInputStream(getClass.getClassLoader.getResourceAsStream("training.tsv"))
-    try {
-      source.getLines().to[Vector] map { parseTestSet }
-    } finally {
-      source.close()
-    }
-  }
+  def loadTrainingSet(): Seq[TrainingSetLine] = load("training.tsv") { parseTrainingSet }
+
+  def loadTestSet(): Seq[TestSetLine] = load("test.tsv") { parseTestSet }
 }
